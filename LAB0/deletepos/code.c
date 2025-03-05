3128a6c1d4e26b4d372107f0b560da2de8397233
Likith200504 <likithtarakatam.cs23@bmsce.ac.in>
2025-03-05 12:25:09 +0530

    Update code.c

--- LAB0/deletepos/code.c
+++ LAB0/deletepos/code.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, n, pos, a[100];
+    printf("ENTER THE NO OF ELEMENTS:");
+    scanf("%d", &n);
+    printf("enter the elements:");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("enter the position to delete:");
+    scanf("%d", &pos);
+    if(pos < 1 || pos > n)
+    {
+        printf("INVALID POSITION");
+    }
+    else
+    {
+        for(i = pos - 1; i < n - 1; i++)
+        {
+            a[i] = a[i + 1];
+        }
+        n--;
+        printf("array after deletion:");
+        for(i = 0; i < n; i++)
+        {
+            printf("%d ", a[i]);
+        }
+    }
+}
+
 
